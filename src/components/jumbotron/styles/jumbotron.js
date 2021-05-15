a8b44c45e0918056b12936aa89cf31c265011626
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-05-14 22:43:29 -0600

    fixed bug to display images properly

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -53,11 +53,13 @@ export const Item = styled.div`
   display: flex;
   border-bottom: 8px solid #222;
   padding: 50px 5%;
-  color: white;
+  /* color: white; */
   overflow: hidden;
 `;
 
 export const Container = styled.div`
+  color: black;
+
   @media (max-width: 1000px) {
     ${Item}:last-of-type h2 {
       margin-bottom: 50px;
